3882ae729c5bde06734550b047d36ea493fb012d
lupeMorales <morales.lupegmail.com>
2022-09-22 22:58:12 +0200

    update hello section

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -2,19 +2,13 @@ import "../styles/layouts/Hello.scss";
 const Hello = () => {
   return (
     <section className="hello">
+      <p className="hello__lupe">lupe morales</p>
       <div className="hello__txt-container">
-        <h1 className="hello__title">
-          hello<span className="hello__point"></span>
-        </h1>
-        <h2 className="hello__static-txt">soy</h2>
-        <ul className="hello__dynamic-txt">
-          <li>
-            <h2>lupe morales</h2>
-          </li>
-          <li>
-            <h2>frontend developer</h2>
-          </li>
-        </ul>
+        <h1 className="hello__title">hello</h1>
+
+        <h2 className="hello__static-txt">
+          soy frontend <span className="hello__point"></span>
+        </h2>
       </div>
     </section>
   );
