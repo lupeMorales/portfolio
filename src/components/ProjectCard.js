044d9a3b5d319451198bea88b01cb1d272565a4b
lupeMorales <morales.lupegmail.com>
2022-09-28 21:16:40 +0200

    add hover effect over proyects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,23 +3,40 @@ const ProjectCard = (props) => {
   const image = require(`../images/${props.projects.img}`);
   return (
     <li className="projectCard">
-      <figure>
-        <img className="projectCard__img" src={image} alt="" />
+      <figure className="projectCard__container">
+        <img
+          className="projectCard__img"
+          src={image}
+          alt={`${props.projects.title}`}
+          title={`${props.projects.title}`}
+        />
+
+        <div className="projectCard__overlay ">
+          <a
+            href={props.projects.code}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="icon"
+          >
+            <span>
+              <i className="fa-solid fa-code fa-4x" title="ver código"></i>
+            </span>
+          </a>
+          <a
+            href={props.projects.demo}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="icon"
+          >
+            <span>
+              <i
+                className="fa-sharp fa-solid fa-eye fa-4x"
+                title="ver demo"
+              ></i>
+            </span>
+          </a>
+        </div>
       </figure>
-      <div className="projectCard__hover ">
-        <p>{props.projects.title}</p>
-        <p>{props.projects.description}</p>
-        <a href={props.projects.code} target="_blank" rel="noopener noreferrer">
-          <span>
-            <i className="fa-solid fa-code"></i>
-          </span>
-        </a>
-        <a href={props.projects.demo} target="_blank" rel="noopener noreferrer">
-          <span>
-            <i className="fa-sharp fa-solid fa-eye"></i>
-          </span>
-        </a>
-      </div>
     </li>
   );
 };
