61ba605b169dc9b1c419d0e233f1f1e15c276733
lupeMorales <morales.lupegmail.com>
2022-09-29 13:48:00 +0200

    add about responsive

--- src/components/About.js
+++ src/components/About.js
@@ -13,12 +13,12 @@ const About = (props) => {
         </section>
         <section className="about__bio">
           <p className="about__text">
-            mi curiosidad innata me hace preguntarme siempre ¿qué hay detrás?
-            ¿cómo se hace? ¿podría hacerlo yo? Y eso me llevó casi sin quererlo
-            al mundo de la programación aunque nunca imaginé que pudiese
-            atraparme como lo hizo, tanto, que tras años dedicada al sector del
-            turismo he decidido dar un giro profesional a mi carrera lanzándome
-            con ilusión y determinación a esta aventura.
+            Mi curiosidad innata me hace preguntarme siempre ¿qué hay detrás?
+            ¿cómo se hace? ¿podría hacerlo yo?<span></span> Eso me llevó casi
+            sin quererlo al mundo de la programación aunque nunca imaginé que
+            pudiese atraparme como lo hizo, tanto, que tras años dedicada al
+            sector del turismo he decidido dar un giro profesional a mi carrera
+            lanzándome con ilusión y determinación a esta aventura.
           </p>
           <img
             src={imgProfile}
@@ -28,11 +28,11 @@ const About = (props) => {
         </section>
         <section className="skills">
           <h3 className="skills__title">Skills</h3>
-          <p className="skills__line">HTML5 CSS3 Sass Flexbox Animaciones</p>
+          <p className="skills__line">HTML5 CSS3 Sass Flexbox Grid</p>
           <p className="skills__line">JavaScript Java React APIs</p>
-          <p className="skills__line">Node-JS Express SQL</p>
+          <p className="skills__line">Node-JS Express-JS SQL</p>
           <p className="skills__line">Git GitHub</p>
-          <p className="skills__line">VS Code Gulp NPM</p>
+          <p className="skills__line">VSCode Gulp NPM</p>
           <p className="skills__line">Agile Scrum</p>
         </section>
       </section>
