32dc5af3f5d5504823cf685ac3c4a5a33c08e696
lupeMorales <morales.lupegmail.com>
2022-10-10 16:45:30 +0200

    add hashRouter, solve index

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -1,26 +1,86 @@
 import "../styles/components/HamburgerMenu.scss";
+import "../styles/components/NavContact.scss";
+
 import { useState } from "react";
-import NavBar from "./NavBar";
+import NavContact from "./NavContact";
+import { Link } from "react-router-dom";
 
 const HamburguerMenu = () => {
   const [toggle, setToggle] = useState(false);
-  const [hambMenuActive, setHambMenuActive] = useState("");
+  const [hambMenuActive, setHambMenuActive] = useState(false);
+  /*   const [hambMenuActive, setHambMenuActive] = useState(""); */
   const handleMenu = (ev) => {
     ev.preventDefault();
     setToggle(!toggle);
-    hambMenuActive === "" ? setHambMenuActive("active") : setHambMenuActive("");
+    setHambMenuActive(!hambMenuActive);
+    /*   hambMenuActive === "" ? setHambMenuActive("active") : setHambMenuActive(""); */
+  };
+  const handleClick = () => {
+    setToggle(!toggle);
+    setHambMenuActive(!hambMenuActive);
   };
 
   return (
     <>
       <a className="navbar" href="menu">
-        <div className={`navbar__icon ${hambMenuActive}`} onClick={handleMenu}>
+        <div
+          className={`navbar__icon ${hambMenuActive ? "active" : ""}`}
+          onClick={handleMenu}
+        >
           <span className={`navbar__line--1 `} id="1"></span>
           <span className={`navbar__line--2 `}></span>
           <span className={`navbar__line--3 `}></span>
         </div>
       </a>
-      {toggle && <NavBar />}
+      {toggle && (
+        <section className="index animate__animated animate__fadeInLeft">
+          <div className="index__container">
+            <ul className="index__items ">
+              <li>
+                <Link
+                  to="/"
+                  target="_parent"
+                  className="index__link"
+                  onClick={handleClick}
+                >
+                  home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/about"
+                  target="_parent"
+                  className="index__link "
+                  onClick={handleClick}
+                >
+                  sobre mi
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/projects"
+                  target="_parent"
+                  className="index__link"
+                  onClick={handleClick}
+                >
+                  proyectos
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/contact"
+                  target="_parent"
+                  className="index__link"
+                  onClick={handleClick}
+                >
+                  contacto
+                </Link>
+              </li>
+            </ul>
+            <NavContact />
+          </div>
+        </section>
+      )}
     </>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,19 +3,43 @@ import "../styles/components/NavContact.scss";
 
 import { Link } from "react-router-dom";
 import NavContact from "./NavContact";
+import { useState } from "react";
 
 function NavBar() {
+  const [hidden, setHidden] = useState(false);
+  const handleClik = () => {
+    setHidden(!hidden);
+  };
+  const displayNone = {
+    display: "none",
+  };
+  const displayBlock = {
+    display: "block",
+  };
   return (
-    <section className="index animate__animated animate__fadeInLeft">
+    <section
+      style={hidden ? displayNone : displayBlock}
+      className="index animate__animated animate__fadeInLeft"
+    >
       <div className="index__container">
         <ul className="index__items ">
           <li>
-            <Link to="/" target="_parent" className="index__link">
+            <Link
+              to="/"
+              target="_parent"
+              className="index__link"
+              onClick={handleClik}
+            >
               home
             </Link>
           </li>
           <li>
-            <Link to="/about" target="_parent" className="index__link ">
+            <Link
+              to="/about"
+              target="_parent"
+              className="index__link "
+              onClick={handleClik}
+            >
               sobre mi
             </Link>
           </li>
